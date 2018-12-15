8440346a34f53614a433347107e6851a53b18f6c
XWxiaowei <2809641033@qq.com>
2018-12-15 22:35:08 +0800

    分页处理，细节完善

---
+++ fly_blog/src/main/java/com/fly/config/MyBatisPlusConfig.java
@@ -0,0 +1,42 @@
+package com.fly.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * MyBatis-plus 分页插件
+ * Created by xiang.wei on 2018/12/15
+ *
+ * @author xiang.wei
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.fly.mapper")
+public class MyBatisPlusConfig {
+
+    /**
+     * 分页插件
+     *
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * sql 执行效率插件
+     * @return
+     */
+    @Bean
+    public PerformanceInterceptor performanceInterceptor(){
+        return new PerformanceInterceptor();
+    }
+
+
+
+}
