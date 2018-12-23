597b81f269d22980b1a380cc34070dbcb0ebc9b7
XWxiaowei <2809641033@qq.com>
2018-12-23 16:09:12 +0800

    相关bug修改

--- fly_blog/src/main/java/com/fly/shiro/AuthFilter.java
+++ fly_blog/src/main/java/com/fly/shiro/AuthFilter.java
@@ -2,6 +2,7 @@ package com.fly.shiro;
 
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.extension.api.R;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.filter.authc.UserFilter;
@@ -17,12 +18,13 @@ import java.io.IOException;
  * @author xiang.wei
  * @create 2018/12/20 16:12
  */
+@Slf4j
 public class AuthFilter extends UserFilter {
 
     @Override
     protected void redirectToLogin(ServletRequest servletRequest, ServletResponse response) throws IOException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-
+        log.info("--------------------->请求接口地址={}", ((HttpServletRequest) servletRequest).getRequestURI());
 //        异步请求要先登录
         String header = request.getHeader("X-Requested-With");
         if (header != null && "XMLHttpRequest".equals(header)) {
@@ -30,10 +32,10 @@ public class AuthFilter extends UserFilter {
             if (!subject.isAuthenticated()) {
                 response.setContentType("application/json;charset=UTF-8");
                 response.getWriter().print(JSONUtil.toJsonStr(R.failed("请先登录!")));
-            } else {
-                super.redirectToLogin(servletRequest, response);
-
             }
+        } else {
+            super.redirectToLogin(servletRequest, response);
+
         }
 
     }
