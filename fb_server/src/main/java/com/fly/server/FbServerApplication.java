73f2a4915e35458eced694ad93b639236d60b902
XWxiaowei <2809641033@qq.com>
2019-02-15 17:59:21 +0800

    分模块五

--- fb_server/src/main/java/com/fly/server/FbServerApplication.java
+++ fb_server/src/main/java/com/fly/server/FbServerApplication.java
@@ -2,8 +2,10 @@ package com.fly.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FbServerApplication {
 
     public static void main(String[] args) {
---
+++ fb_server/src/main/java/com/fly/server/config/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+//package com.fly.server.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @author xiang.wei
+// * 添加了权限的校验之后增加一个关闭csrf的配置，
+// * 防止其他服务注册不上去。
+// */
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+////        关闭csrf
+//        http.csrf().ignoringAntMatchers("/eureka/**");
+//        super.configure(http);
+//    }
+//}
