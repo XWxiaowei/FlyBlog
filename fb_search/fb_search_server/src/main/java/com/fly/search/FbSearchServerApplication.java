fb555779ef116b38a86d2cf91bc0ae402265ab77
XWxiaowei <2809641033@qq.com>
2019-02-16 14:28:10 +0800

    添加ES

---
+++ fb_search/fb_search_server/src/main/java/com/fly/search/FbSearchServerApplication.java
@@ -0,0 +1,14 @@
+package com.fly.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbSearchServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbSearchServerApplication.class, args);
+	}
+
+}
+
--- fb_search/fb_search_server/src/test/java/com/fly/search/FbSearchServerApplicationTests.java
+++ fb_search/fb_search_server/src/test/java/com/fly/search/FbSearchServerApplicationTests.java
@@ -7,11 +7,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class FbSearchApplicationTests {
+public class FbSearchServerApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }
 
